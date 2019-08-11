0daa0bc1176d11562ca6b195c85ddf2099bc6bda
xfaxca <Cameron@tutanota.com>
2019-08-11 18:12:35 -0500

    - Added tokenizer generator function

--- pymlkit/nlp/tokenize.py
+++ pymlkit/nlp/tokenize.py
@@ -2,11 +2,39 @@
 Module for tokenization and punctuation treatment.
 """
 import re
+import pandas as pd
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 
+__all__ = [
+    'iterate_tokens_from_df',
+    'simple_tokenize',
+    'remove_punctuation'
+]
 
-def simple_tokenize(text, strip_punct=False, stopwords=None):
+
+def iterate_tokens_from_df(df, idcol, textcol, tokenizer_fn=None, **kwargs):
+    """
+    Returns a generator of id:token pairs, with the tokens being the result of the application of the specified
+        tokenizer function to the specified text column in the dataframe.
+    :param df: (pd.DataFrame)
+    :param idcol: (str) - Name of the column where document/string ids are located.
+    :param textcol: (str) - Name of the column where the text to be tokenized is located.
+    :param tokenizer_fn: (function) - Function/callable that will be used for tokenization.
+    :param strip_punct: (whether or not to strip punctuation out of the text prior to tokenization. NOTE: The tokenizer
+        in use ust support the keyword 'strip_punct` for this to have any impact.
+    :keywords: Keywords to be passed to the tokenizer being used.
+    :return:
+    """
+    tokenize = tokenizer_fn if tokenizer_fn else simple_tokenize
+    for id_tag, text in zip(df[idcol], df[textcol]):
+        if pd.isnull(text):
+            continue
+        tokens = tokenize(text, **kwargs)
+        yield id_tag, tokens
+
+
+def simple_tokenize(text, strip_punct=True, stopwords=None):
     """
     Tokenization function, with option to strip
     """
